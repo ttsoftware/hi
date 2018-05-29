4bcd7d7b9b15a3afbb96f257633b96ea5465d7d5
Troels Thomsen <tt@ttsoftware.net>
2018-05-29 11:54:59 +0200

    try again

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,12 @@
 using namespace dlib;
 using namespace std;
 
+// The following defines a ResNet network.
+// The dlib_face_recognition_resnet_model_v1 model used by this example was trained using
+// essentially the code shown in dnn_metric_learning_on_images_ex.cpp except the
+// mini-batches were made larger (35x15 instead of 5x5), the iterations without progress
+// was set to 10000, and the training dataset consisted of about 3 million images instead of
+// 55.  Also, the input layer was locked to images of size 150.
 template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
 using residual = add_prev1<block<N,BN,1,tag1<SUBNET>>>;
 
@@ -29,16 +35,22 @@ template <typename SUBNET> using alevel3 = ares<64,ares<64,ares<64,ares_down<64,
 template <typename SUBNET> using alevel4 = ares<32,ares<32,ares<32,SUBNET>>>;
 
 using anet_type = loss_metric<fc_no_bias<128,avg_pool_everything<
-        alevel0<
-        alevel1<
-        alevel2<
-        alevel3<
-        alevel4<
-        max_pool<3,3,2,2,relu<affine<con<32,7,7,2,2,
-                input_rgb_image_sized<150>
-        >>>>>>>>>>>>;
+                            alevel0<
+                            alevel1<
+                            alevel2<
+                            alevel3<
+                            alevel4<
+                            max_pool<3,3,2,2,relu<affine<con<32,7,7,2,2,
+                            input_rgb_image_sized<150>
+                            >>>>>>>>>>>>;
 
-int main() {
+// The following defines jitters for face encoding
+
+std::vector<matrix<rgb_pixel>> jitter_image(
+        const matrix<rgb_pixel> &img
+);
+
+int main() try {
 
     // The first thing we are going to do is load all our models.  First, since we need to
     // find faces in the image we will need a face detector:
@@ -53,12 +65,55 @@ int main() {
     deserialize("data/models/dlib_face_recognition_resnet_model_v1.dat") >> net;
 
     matrix<rgb_pixel> img;
-    load_image(img, "data/faces/troels.jpg");
+    load_image(img, "data/faces/troels.png");
     // Display the raw image on the screen
-    image_window win(img);
+    // image_window win(img);
 
+    // Run the face detector on the image of our action heroes, and for each face extract a
+    // copy that has been normalized to 150x150 pixels in size and appropriately rotated
+    // and centered.
 
-    // std::cout << "Hello, World!" << std::endl;
+    std::vector<matrix<rgb_pixel>> faces;
+    for (auto face : detector(img)) {
+        auto shape = sp(img, face);
+        matrix<rgb_pixel> face_chip;
+        extract_image_chip(img, get_face_chip_details(shape, 150, 0.25), face_chip);
+        faces.push_back(move(face_chip));
+    }
+
+    if (faces.size() == 0) {
+        cout << "No faces found in image!" << endl;
+        return 1;
+    }
+    /*
+        // This call asks the DNN to convert each face image in faces into a 128D vector.
+        // In this 128D vector space, images from the same person will be close to each other
+        // but vectors from different people will be far apart.  So we can use these vectors to
+        // identify if a pair of images are from the same person or from different people.
+        std::vector<matrix<float, 0, 1>> face_descriptors = net(faces);
+        std::cout << "face descriptor for one face: " << trans(face_descriptors[0]) << endl;
+
+        matrix<float, 0, 1> face_descriptor = mean(mat(net(jitter_image(faces[0]))));
+        std::cout << "jittered face descriptor for one face: " << trans(face_descriptor) << endl;
+       */
 
     return 0;
+}
+catch (std::exception &e) {
+    cout << e.what() << endl;
+}
+
+std::vector<matrix<rgb_pixel>> jitter_image(
+        const matrix<rgb_pixel> &img
+) {
+    // All this function does is make 100 copies of img, all slightly jittered by being
+    // zoomed, rotated, and translated a little bit differently. They are also randomly
+    // mirrored left to right.
+    thread_local dlib::rand rnd;
+
+    std::vector<matrix<rgb_pixel>> crops;
+    for (int i = 0; i < 100; ++i)
+        crops.push_back(jitter_image(img, rnd));
+
+    return crops;
 }
