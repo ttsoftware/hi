825bf3bcd457d56b55a9194fe5c33c278a707e2f
Troels Thomsen <tt@ttsoftware.net>
2018-05-30 20:06:57 +0200

    Removes worskspace.

--- src/main.cpp
+++ src/main.cpp
@@ -18,20 +18,16 @@ int main() try {
 
     // initialize neural networks
     auto hi = Hi();
-
     auto time1 = (duration_cast<milliseconds>(system_clock::now().time_since_epoch())).count();
 
     cout << "Loaded neural networks in " << time1 - time0 << " ms" << endl;
 
-    /*auto descriptor = hi.createDescriptor("data/faces/troels.png", 25);
-
+    /*auto descriptor = hi.createDescriptor("data/faces/troels.png", 100);
     cout << "Found a face descriptor." << endl;
-
     hi.storeDescriptor(descriptor, "data/face_descriptors/troels.dat");
-    */
+    return 0;*/
 
     auto troels_descriptor = hi.loadDescriptor("data/face_descriptors/troels.dat");
-
     auto time2 = (duration_cast<milliseconds>(system_clock::now().time_since_epoch())).count();
 
     cout << "Loaded Troels face descriptor in " << time2 - time1 << " ms" << endl;
@@ -46,9 +42,6 @@ int main() try {
             cout << "\nComparing with " << path << endl;
 
             auto descriptors = hi.getDescriptors(path.path().string());
-
-            // cout << "Found " << descriptors.size() << " other face descriptors." << endl;
-
             auto result = hi.contains(descriptors, troels_descriptor, 0.5);
 
             if (result) {
