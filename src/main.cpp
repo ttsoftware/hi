ac3cfa3f8a81afcaa331cc693f527263699b390c
Troels Thomsen <tt@ttsoftware.net>
2018-06-03 21:35:36 +0200

    almost done with cli

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include <experimental/filesystem>
 #include <chrono>
 #include <iterator>
 #include <vector>
 #include <thread>
+#include <string>
+#include <fstream>
+#include <experimental/filesystem>
 
 #include <dlib/opencv.h>
 #include <dlib/opencv/cv_image.h>
@@ -26,11 +28,9 @@ auto time0 = (duration_cast<milliseconds>(system_clock::now().time_since_epoch()
 auto hi = Hi();
 auto time1 = (duration_cast<milliseconds>(system_clock::now().time_since_epoch())).count();
 
-int recognize(const string &reference_descriptor_name) try {
+int recognize(const string &reference_descriptor_path) {
 
-    cout << "Loaded neural networks in " << time1 - time0 << " ms" << endl;
-
-    auto troels_descriptor = hi.loadDescriptor("data/face_descriptors/" + reference_descriptor_name + ".dat");
+    auto troels_descriptor = hi.loadDescriptor(reference_descriptor_path);
     auto time2 = (duration_cast<milliseconds>(system_clock::now().time_since_epoch())).count();
 
     cout << "Loaded reference face descriptor in " << time2 - time1 << " ms" << endl;
@@ -49,21 +49,51 @@ int recognize(const string &reference_descriptor_name) try {
     bool result = hi.contains(descriptors, troels_descriptor, 0.5);
 
     auto time5 = (duration_cast<milliseconds>(system_clock::now().time_since_epoch())).count();
+
     cout << "Creating face descriptors in " << time5 - time4 << " ms." << endl;
     cout << (result ? "Success" : "Failure") << endl;
-    cout << "Total time " << (time5 - time0) / 1000 << " s." << endl;
+    cout << "Auth time " << (time5 - time2) / 1000 << " s." << endl;
 
-    return 0;
-}
-catch (std::exception &e) {
-    cout << e.what() << endl;
-    return 1;
+    return result ? 0 : 1;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) try {
+    cout << "Loaded neural networks in " << time1 - time0 << " ms" << endl;
+
     if (argc == 1) {
         // start daemon
-        return 1;
+        pid_t parent_pid = getpid();
+        pid_t child_pid = fork();
+
+        if (child_pid == 0) {
+            // open pipe and wait forever
+
+            const char *fifo = "/tmp/hi_fifo";
+            mkfifo(fifo, 0666);
+            ifstream fifo_stream(fifo);
+            fifo_stream.ignore();
+
+            string command;
+            auto descriptor_folder = std::experimental::filesystem::u8path("data/face_descriptors");
+            while (true) {
+                fifo_stream >> command;
+                if (command.compare("auth")) {
+                    // authenticate against existing stored descriptors
+                    for (auto &path : std::experimental::filesystem::directory_iterator(descriptor_folder)) {
+                        if (recognize(path.path().string()) == 0) break;
+                    }
+                }
+
+                // clear fifo for next command
+                /*char* buffer[16];
+                auto fd = fopen(fifo, "r");
+                fread(buffer, 1, 15, fd);*/
+            }
+        } else {
+            cout << "Successfully started Hi daemon on pid " << child_pid << endl;
+        }
+
+        return 0;
     }
     if (argc == 2) {
         // recognize face
@@ -101,9 +131,10 @@ int main(int argc, char **argv) {
                 return 1;
             }
             cout << "Done!" << endl;
-        } else {
         }
     }
 
     return 1;
+} catch (std::exception &e) {
+    cout << e.what() << endl;
 }
